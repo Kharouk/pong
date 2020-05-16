e0d075c95c1cf1b796de9459ba5d1a098fa5dd75
Alex Kharouk <akharoukpp@gmail.com>
2020-05-16 09:26:07 +0100

    Make Sphere bounce to the side and set up ceiling

---
+++ Assets/Scripts/Sphere.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sphere : MonoBehaviour
+{
+    public float bounciness = 1;
+    public float speed = 3f;
+    private Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(transform.position.x * new Vector3(speed, 0, 0));
+    }
+}
