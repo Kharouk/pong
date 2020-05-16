901a24a1ce7fac4c0c3e11ce4bc23b84fc2905dc
Alex Kharouk <akharoukpp@gmail.com>
2020-05-16 08:30:13 +0100

    initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    void Start()
+    {
+        Debug.Log("Hello Test!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
