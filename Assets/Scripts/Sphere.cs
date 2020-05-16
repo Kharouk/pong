0357f485fe6c1a100ce34d39e11385606ec800d1
Alex Kharouk <akharoukpp@gmail.com>
2020-05-16 18:03:45 +0100

    Working on Enemy paddle following the ball's Y axis.

--- Assets/Scripts/Sphere.cs
+++ Assets/Scripts/Sphere.cs
@@ -6,11 +6,10 @@ using Random = UnityEngine.Random;
 
 public class Sphere : MonoBehaviour
 {
-    
     public float bounciness = 1;
     private Vector3 speed = new Vector3(300, 15);
     private Rigidbody ball_rb;
-    
+
     private void StartRandom()
     {
         float rand = Random.Range(0, 2);
@@ -24,28 +23,41 @@ public class Sphere : MonoBehaviour
             ball_rb.AddForce(-speed);
         }
     }
-    
+
     void Start()
     {
         ball_rb = GetComponent<Rigidbody>();
         StartRandom();
     }
-    
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.CompareTag("Player"))
         {
             Vector3 vel = new Vector3();
             var ballVelocity = ball_rb.velocity;
-            
+
             var player = collision.collider.attachedRigidbody.velocity.y;
 
             vel.x = ballVelocity.x * 1.1f; // Increases the speed of the ball velocity
-            vel.y = (ballVelocity.y * 1.1f) + (player /  3.0f);
+            vel.y = (ballVelocity.y * 1.1f) + (player / 3.0f);
+
+            ball_rb.velocity = vel;
+        }
+
+        if (collision.collider.CompareTag("Enemy"))
+        {
+            Vector3 vel = new Vector3();
+            var ballVelocity = ball_rb.velocity;
+
+            var enemy = collision.collider.attachedRigidbody;
+
+            vel.x = ballVelocity.x * 1.1f; // Increases the speed of the ball velocity
+            vel.y = (ballVelocity.y * 1.1f) + (enemy.velocity.y / 3.0f);
 
             ball_rb.velocity = vel;
 
+            enemy.velocity.y = ball_rb.velocity.y;
         }
-        Debug.Log(collision.collider.tag);
     }
 }
