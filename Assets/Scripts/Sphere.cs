794ea578e61ca0dd8ddbc4bbab93667fbc483243
Alex Kharouk <akharoukpp@gmail.com>
2020-05-16 12:44:31 +0100

    Begin working on collision system for ball

--- Assets/Scripts/Sphere.cs
+++ Assets/Scripts/Sphere.cs
@@ -1,20 +1,51 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Sphere : MonoBehaviour
 {
+    
     public float bounciness = 1;
-    public float speed = 3f;
-    private Rigidbody rb;
+    private Vector3 speed = new Vector3(500, 5, 0);
+    private Rigidbody ball_rb;
+    
+    private void StartRandom()
+    {
+        float rand = Random.Range(0, 2);
+        Debug.Log(rand);
+        if (rand < 1)
+        {
+            ball_rb.AddForce(speed);
+        }
+        else
+        {
+            ball_rb.AddForce(-speed);
+        }
+    }
+    
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        ball_rb = GetComponent<Rigidbody>();
+        StartRandom();
     }
 
     // Update is called once per frame
     void Update()
     {
-        rb.AddForce(transform.position.x * new Vector3(speed, 0, 0));
+         // speed.x = Mathf.Clamp(speed.x, 0, 20);
+         // ball_rb.AddForce(speed);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            Vector3 vel = new Vector3();
+            vel.x = ball_rb.velocity.x;
+            Debug.Log(vel);
+        }
+        Debug.Log(collision.collider.tag);
     }
 }
